42856af55edb6362dd398000b3058fa8b080f2c2
piyush <Piyushsuthar524@gmail.com>
2026-04-04 00:02:40 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "Installing dependencies..."
-npm install
-
 echo "Installing Chrome for Puppeteer..."
 npx puppeteer browsers install chrome
 
-echo "Running build..."
+echo "Running TypeScript build..."
 npm run build
 
 echo "Build completed!"
